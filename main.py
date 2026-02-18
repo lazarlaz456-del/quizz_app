3c833b18c781a5837c7e7ca1961be39c0cea9901
Voja <lazarlaz456@gmail.com>
2026-02-18 22:18:38 +0100

    Developed additional functionality for start button

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Definition of App class
 import tkinter as tk
+from tkinter import messagebox
 import os
 import requests as rq
 import html
@@ -7,42 +8,70 @@ import html
 base_dir_path = os.path.dirname(os.path.abspath(__file__))
 
 class App(tk.Tk):
+    #Initialize the window
     def __init__(self):
         super().__init__()
         self.title("Welcome to the Quizzler")
         self.config(padx=3, pady=3)
 
+        #Initialize the canvas
         self.canvas = tk.Canvas(self, bg="#D3D3D3", height=500, width=500)
         self.canvas.grid(row=0, column=0, rowspan=5, columnspan=5, sticky="nsew")
 
+        #Initialize the start button
         self.start_button = tk.Button(self, text="Start", font=("Times New Roman", 18, "italic"), bg="#45A049", fg="white",
-                                    relief="flat", activebackground="#43A047", activeforeground="black", command=self.get_question_bank)
+                                    relief="flat", activebackground="#43A047", activeforeground="black", command=self.start_or_restart_game)
         self.start_button.grid(row=0, column=1, padx=10, pady=10)
 
+        #Initialize the quit button
         self.quit_button = tk.Button(self, text="Quit", font=("Times New Roman", 18, "italic"), bg="black", fg="white",
                                     relief="flat", activebackground="#333333", activeforeground="white")
         self.quit_button.grid(row=0, column=3, padx=10, pady=10)
 
+        #Initialize text area where questions will be displayed
         self.text_area = tk.Text(self, height=10, wrap="word")
         self.text_area.grid(row=1, column=1, padx=10, pady=10, columnspan=3, rowspan=3, sticky="nsew")
 
+        #Initialize True button
         img_path_true = os.path.join(base_dir_path, "true.png")
         self.true_img = tk.PhotoImage(file=img_path_true)
         self.true_button = tk.Button(self, image=self.true_img, command=self.true_button_choice)
         self.true_button.grid(row=4, column=1, padx=10, pady=10)
 
+        #Initialize False button
         img_path_false = os.path.join(base_dir_path, "false.png")
         self.false_img = tk.PhotoImage(file=img_path_false)
         self.false_button = tk.Button(self, image=self.false_img, command=self.false_button_choice)
         self.false_button.grid(row=4, column=3, padx=10, pady=10)
 
+        #Initialize score label
         self.score = 0
         self.score_label = tk.Label(self, text=f"SCORE\n{self.score}/10", anchor="center", font=("Times New Roman", 20, "bold"), bg="#D3D3D3")
         self.score_label.grid(row=4, column=2, padx=10, pady=10)
 
+        #Initialize question number label
         self.question_label = tk.Label(self, text="", anchor="center", font=("Times New Roman", 20, "bold"), bg="#D3D3D3")
         self.question_label.grid(row=0, column=2, padx=10, pady=10)
 
+        self.question_key = 1
+
+    #Function to start/restart game - used with the start button
+    def start_or_restart_game(self):
+
+        if self.question_key == 1:
+            self.get_question_bank()
+        else:
+
+            if 1 < self.question_key <= 10:
+                restart_game = messagebox.askyesno("Restart game?", "You have pressed start while a quiz is already running, do you wish to restart game?")
+
+            if self.question_key > 10 or restart_game:
+                self.score = 0
+                self.question_key = 1
+                self.get_question_bank()
+
+
+    #Function to get the question bank using API
     def get_question_bank(self):
         url = "https://opentdb.com/api.php?amount=10&type=boolean"
         response = rq.get(url)
@@ -60,24 +89,28 @@ class App(tk.Tk):
             counter_key += 1
         
         self.question_bank = questions_clean
-        self.question_key = 1
         self.current_question = self.question_bank[self.question_key]["question"]
 
         self.text_area.delete("1.0", "end")
         self.text_area.insert("end", self.current_question)
 
         self.question_label.config(text=f"Question {self.question_key}")
+        self.score_label.config(text=f"SCORE\n{self.score}/10")
+
         self.true_button.config(state="normal")
         self.false_button.config(state="normal")
 
+    # Function to get the choice and update score if user picked True
     def true_button_choice(self):
         self.choice = "True"
         self.get_result_next_question()
 
+    #Same for False
     def false_button_choice(self):
         self.choice = "False"
         self.get_result_next_question()
 
+    #Function to iterate over the question bank and select the next question
     def get_result_next_question(self):
         if self.choice == self.question_bank[self.question_key]["answer"]:
             text_to_write = "Bravo! Your answer is correct!"
@@ -88,8 +121,9 @@ class App(tk.Tk):
         self.text_area.delete("1.0", "end")
         self.text_area.insert("end", text_to_write)
 
-        self.after(2000, self.next_question)
+        self.after(1500, self.next_question)
 
+    #Function to switch to next question and to stop the game once all questions have been answered
     def next_question(self):
         try:
             self.question_key += 1
@@ -105,6 +139,8 @@ class App(tk.Tk):
             self.text_area.delete("1.0", "end")
             self.text_area.insert("end", self.current_question)
             self.question_label.config(text=f"Question {self.question_key}")
+    
+    
 
 app = App()
 
